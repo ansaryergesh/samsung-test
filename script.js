e89e1199ec45fe35587a1c7d1ac401672f311147
ansaryergesh <yergeshovansar@gmail.com>
2020-09-07 23:03:17 +0600

    validation

--- script.js
+++ script.js
@@ -15,8 +15,12 @@ function addPost(event) {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            Accept: 'application/json',
-            'Access-Control-Allow-Origin': '*',
+            "Accept": 'application/json',
+            'Access-Control-Allow-Origin': "https://foo.example",
+            "Access-Control-Max-Age": 86400,
+            "Access-Control-Allow-Credentials": true,
+            "Access-Control-Request-Method": "POST",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
         },
         body: JSON.stringify(myPost)
     })
@@ -31,3 +35,46 @@ function addPost(event) {
     .then((data) => console.log(data))
     .catch(err => console.log('Error message:', err.statusText));
 }
+let regex = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+// validation
+$("#contact-form").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      fio: {
+        required: true,
+        minlength: 5  
+        },
+      gorod: {
+          required: true
+      },
+      phone: {
+          required: true,
+          number: true 
+      }
+      },
+    messages: {
+      email: {
+        required: "Поле email обязательно для заполнения",
+        email : "Email неправильно"
+      },
+      fio: {
+        required: "Поле ФИО обязательно для заполнения",
+        minlength: jQuery.validator.format("Длина имени должна быть больше 5-ти символов")
+      },
+      phone: {
+        required: "Поле Номер телефона обязательно для заполнения",
+        number : "Неправильный номер"
+      },
+      gorod: {
+        required: "Поле Город обязательно для заполнения"
+      },
+
+    },
+     submitHandler: function() {
+       alert("Вы успешно зарегистрировались!");
+    }
+  });
+// var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
